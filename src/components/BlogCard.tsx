ca40792381f38392361df0419e7d808224bbad54
tiwariParth <243mjparth@gmail.com>
2024-04-01 19:33:15 +0530

    Refactor BlogCard and Blogs components

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -7,8 +7,41 @@ interface BlogCardProps {
   content: string;
 }
 
-const BlogCard = ({title}) => {
-  return <div></div>;
+const BlogCard = ({
+  title,
+  authorName,
+  publishedAt,
+  content,
+}: BlogCardProps) => {
+  return (
+    <div className="border-b p-4 pb-2 border-slate-200">
+      <div className="flex gap-2 items-center pb-2 ">
+        <div className="flex flex-col justify-center">
+          <Avatar authorName={authorName} />
+        </div>
+        <h6 className="font-extralight text-sm  text-gray-600">{authorName}</h6>
+        <span className="font-extralight text-sm text-gray-600">
+          . {publishedAt}
+        </span>
+      </div>
+      <div className="font-semibold text-xl ">{title}</div>
+
+      <div className="text-md font-thin">{content.slice(0, 100) + "..."}</div>
+      <div className="text-slate-400 pt-3 text-sm font-thin">{`${Math.ceil(
+        content.length / 100
+      )} minute`}</div>
+    </div>
+  );
+};
+
+const Avatar = ({ authorName }: { authorName: string }) => {
+  return (
+    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+      <span className="font-thin text-gray-600 dark:text-gray-300">
+        {authorName[0]}
+      </span>
+    </div>
+  );
 };
 
 export default BlogCard;
--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import BlogCard from "../components/BlogCard";
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className="flex flex-col gap-2 justify-center items-center">
+      <div className="max-w-xl">
+        <BlogCard
+          title={"How to make a blog using React and TailwindCSS"}
+          authorName={"Parth"}
+          publishedAt={"21 Feb 2024"}
+          content={
+            "This is a blog post about how to make a blog using React and TailwindCSS. It's a very simple process that only takes a few minutes to set up. You can use this blog post as a template for your own blog, or you can customize it to fit your needs. Either way, I hope you find this blog post helpful!"
+          }
+        />
+      </div>
+      <div className=" max-w-xl">
+        <BlogCard
+          title={"How to make a blog using React and TailwindCSS"}
+          authorName={"Parth"}
+          publishedAt={"21 Feb 2024"}
+          content={
+            "This is a blog post about how to make a blog using React and TailwindCSS. It's a very simple process that only takes a few minutes to set up. You can use this blog post as a template for your own blog, or you can customize it to fit your needs. Either way, I hope you find this blog post helpful!"
+          }
+        />
+      </div>
+      <div className=" max-w-xl">
+        <BlogCard
+          title={"How to make a blog using React and TailwindCSS"}
+          authorName={"Parth"}
+          publishedAt={"21 Feb 2024"}
+          content={
+            "This is a blog post about how to make a blog using React and TailwindCSS. It's a very simple process that only takes a few minutes to set up. You can use this blog post as a template for your own blog, or you can customize it to fit your needs. Either way, I hope you find this blog post helpful!"
+          }
+        />
+      </div>
+      <div className=" max-w-xl">
+        <BlogCard
+          title={"How to make a blog using React and TailwindCSS"}
+          authorName={"Parth"}
+          publishedAt={"21 Feb 2024"}
+          content={
+            "This is a blog post about how to make a blog using React and TailwindCSS. It's a very simple process that only takes a few minutes to set up. You can use this blog post as a template for your own blog, or you can customize it to fit your needs. Either way, I hope you find this blog post helpful!"
+          }
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
